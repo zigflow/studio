b102858933672bf6fb1074b0f717ac43648cc6b4
Simon Emms <simon@simonemms.com>
2026-07-16 08:52:38 +0000

    chore: create sveltekit app
    Signed-off-by: Simon Emms <simon@simonemms.com>

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025 - 2026 Zigflow authors <https://github.com/zigflow/studio/graphs/contributors>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import adapter from '@sveltejs/adapter-node';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    sveltekit({
+      compilerOptions: {
+        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+        runes: ({ filename }) =>
+          filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+      },
+
+      adapter: adapter(),
+    }),
+  ],
+  server: {
+    host: process.env.HOST ?? '0.0.0.0',
+    port: Number(process.env.PORT ?? 5173),
+  },
+});
